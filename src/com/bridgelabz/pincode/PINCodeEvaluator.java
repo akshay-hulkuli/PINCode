6603c5098b542abc2f026bbc14c96d150db112ac
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-07 17:40:32 +0530

    ADDED: method to check 6 digits in pincode

---
+++ src/com/bridgelabz/pincode/PINCodeEvaluator.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.pincode;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class PINCodeEvaluator {
+	
+	public static boolean pinCodeChecker(String pincode) {
+		Pattern pattern = Pattern.compile("[0-9]{6}");
+		Matcher matcher = pattern.matcher(pincode);
+		return matcher.matches();
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("PINCode Evaluator \nenter a pin code");
+		Scanner sc = new Scanner(System.in);
+		String pincode = sc.next();
+		
+		if(pinCodeChecker(pincode))System.out.println("It is a valid pincode");
+		else System.out.println("It is an invalid pincode");
+
+	}
+
+}
